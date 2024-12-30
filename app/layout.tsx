9585ebffa0fadadca9e6da4f746e6e0dc3c97f79
Jef Fasseur <accounts@jeffasseur.be>
2024-12-30 14:32:26 +0100

    light theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/Header";
-import { ThemeProvider } from "@/components/theme-provider";
 // import { CartProvider } from "@/cosmic/blocks/ecommerce/CartProvider";
 import { TailwindIndicator } from "@/components/tailwind-indicator";
 import { Suspense } from "react";
@@ -39,38 +38,30 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" className="light" suppressHydrationWarning>
       <body
         className={`${plusJakartaSans.variable} font-sans md:p-0 bg-white dark:bg-dark-90 dark:text-light-90 h-dvh w-full`}
       >
         <Suspense>
           <AuthProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-              themes={["light", "dark"]}
-            >
-              {/* <CartProvider> */}
-              <div>
-                <Header />
-                {children}
-              </div>
-              <Footer />
-              <CookieConsent />
-              <Analytics />
-              <SpeedInsights />
-              {/* </CartProvider> */}
-              {
-                // only in dev environment
-                process.env.NODE_ENV === "development" && (
-                  <>
-                    <TailwindIndicator />
-                  </>
-                )
-              }
-            </ThemeProvider>
+            {/* <CartProvider> */}
+            <div>
+              <Header />
+              {children}
+            </div>
+            <Footer />
+            <CookieConsent />
+            <Analytics />
+            <SpeedInsights />
+            {/* </CartProvider> */}
+            {
+              // only in dev environment
+              process.env.NODE_ENV === "development" && (
+                <>
+                  <TailwindIndicator />
+                </>
+              )
+            }
           </AuthProvider>
         </Suspense>
       </body>
