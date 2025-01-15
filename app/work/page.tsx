8121d06bed15f0b85dc0a6f6ef6a95e546e3bb64
Jef Fasseur <accounts@jeffasseur.be>
2025-01-15 22:16:01 +0100

    remove search field

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -32,34 +32,6 @@ export default async function WorkPage() {
               className="text-xl text-zinc-700 dark:text-zinc-300"
             />
           </div>
-          <div>
-            <div className="flex">
-              <input
-                type="text"
-                className="p-2 rounded-md border border-light-90"
-              />
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-light-90"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M17.5 17.5l5 5"
-                />
-              </svg>
-            </div>
-          </div>
           <div className="mt-6 w-full grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 xl:gap-x-8">
             {projects.map((project: ProjectType) => {
               return <ProjectCard key={project.id} project={project} />;
