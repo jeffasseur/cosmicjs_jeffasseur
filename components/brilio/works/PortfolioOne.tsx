34581c0c56c6edab9c3ac31b6ef2f4fb3857ebc7
Jef Fasseur <accounts@jeffasseur.be>
2025-07-01 20:39:25 +0200

    good slug

--- components/brilio/works/PortfolioOne.tsx
+++ components/brilio/works/PortfolioOne.tsx
@@ -13,28 +13,44 @@ const PortfolioOne = async () => {
 
   return (
     <div className={`mx-0 flex flex-wrap mt-0`}>
-      <div className={`${styles.stackWrapper} flex-shrink-0 w-full max-w-full mt-0 px-20`}>
+      <div
+        className={`${styles.stackWrapper} flex-shrink-0 w-full max-w-full mt-0 px-20`}
+      >
         {projects.map((item: ProjectType) => (
           <div className={`${styles.stackItem} cursor-pointer`} key={item.id}>
-            <div className={`${styles.portfolioItem} ${styles.card} ${styles.layout2} ${styles.hasShadow}`}>
+            <div
+              className={`${styles.portfolioItem} ${styles.card} ${styles.layout2} ${styles.hasShadow}`}
+            >
               <div className={`${styles.imageHolder}`}>
                 <a className={`${styles.cardThumb}`} href="/portfolio-single">
-                  <img src={item.metadata.image.imgix_url} alt={item.title} className={`min-h-[550px] object-cover transition-all duration-500`} />
+                  <img
+                    src={item.metadata.image.imgix_url}
+                    alt={item.title}
+                    className={`min-h-[550px] object-cover transition-all duration-500`}
+                  />
                 </a>
                 <div className={`${styles.cardOverlay}`}>
                   <div className={`${styles.heading}`}>
-                    <h4 className={`${styles.title} mt-2 md:mt-3 mb-3`}>{item.title}</h4>
+                    <h4 className={`${styles.title} mt-2 md:mt-3 mb-3`}>
+                      {item.title}
+                    </h4>
                     <div className={`${styles.showProject}`}>
                       <div className={`${styles.cardTerms}`}>
-                        <a className={`${styles.terms} ${styles.badge} ${styles.outlined}`} href="/portfolio">
+                        <a
+                          className={`${styles.terms} ${styles.badge} ${styles.outlined}`}
+                          href="/portfolio"
+                        >
                           {item.metadata.category.title}
                         </a>
-                        <a className={`${styles.terms} ${styles.badge} ${styles.outlined}`} href="/portfolio">
+                        <a
+                          className={`${styles.terms} ${styles.badge} ${styles.outlined}`}
+                          href="/portfolio"
+                        >
                           {item.metadata.client.title}
                         </a>
                       </div>
                       <div className={`${styles.projectLink}`}>
-                        <a href="/portfolio-single">Show Project</a>
+                        <a href={`/work/${item.slug}`}>Show Project</a>
                       </div>
                     </div>
                   </div>
