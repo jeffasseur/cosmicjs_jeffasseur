cfd596d3505f11bb0f6541e2b0f19d1a7e80a652
Jef Fasseur <accounts@jeffasseur.be>
2025-08-02 21:55:25 +0200

    .

--- cosmic/blocks/blog/SingleBlog copy.tsx
+++ cosmic/blocks/blog/SingleBlog copy.tsx
@@ -41,30 +41,32 @@ export async function SingleBlogCopy({
 
             {/* Top section with title and short description */}
             <div className="py-4 flex flex-col lg:flex-row lg:items-end lg:justify-between lg:gap-12 lg:mb-6 lg:py-10 w-full">
-              <div className="flex flex-col gap-2 lg:gap-6">
+              <div className="flex flex-col gap-2 lg:gap-6 lg:w-3/5">
                 {/* Left section: date, title, buttons */}
-                <span className="font-extrabold text-light-50 text-sm">{date}</span>
-                <h1 className="text-3xl font-bold leading-tight tracking-tighter text-primary w-full dark:text-white sm:text-4xl md:text-5xl dark:font-semibold dark:tracking-tight">
+                <span className="font-extrabold text-light-50 text-sm">
+                  {date}
+                </span>
+                <h1 className="text-3xl font-bold leading-snug tracking-tighter text-primary w-full dark:text-white sm:text-4xl md:text-5xl dark:font-semibold dark:tracking-tight">
                   {blog.title}
                 </h1>
                 <div className="flex flex-wrap gap-2">
                   {blog.metadata.categories.map((category: any) => {
-              return (
-                <span
-                  className="py-2 px-4 border border-light-50 rounded-full mr-2 text-xs font-semibold uppercase tracking-wide text-light-50 dark:text-light-70 dark:border-light-70"
-                  key={category.slug}
-                >
-                  {category.title}
-                </span>
-              );
-            })}
+                    return (
+                      <span
+                        className="py-2 px-4 border border-light-50 rounded-full mr-2 text-xs font-semibold uppercase tracking-wide text-light-50 dark:text-light-70 dark:border-light-70"
+                        key={category.slug}
+                      >
+                        {category.title}
+                      </span>
+                    );
+                  })}
                 </div>
               </div>
 
-              <div>
+              <div className="lg:w-2/5">
                 <p className="line-clamp-4 w-full pt-3 lg:text-lg text-dark-80 dark:text-light-70">
-              {blog.metadata.content.slice(0, 250)}...
-            </p>
+                  {blog.metadata.content.slice(0, 250)}...
+                </p>
               </div>
             </div>
             <div className="mb-10 w-full overflow-hidden rounded-xl">
